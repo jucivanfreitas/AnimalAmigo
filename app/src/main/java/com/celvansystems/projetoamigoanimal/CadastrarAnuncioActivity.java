82f8992dd2ff6d8f38cba25b3b62ef692cce5a3f
Celio <celioalves10@hotmail.com>
2019-01-28 22:53:46 -0300

    content main

--- app/src/main/java/com/celvansystems/projetoamigoanimal/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/CadastrarAnuncioActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.example.projetoamigoanimal.R;
+import com.celvansystems.projetoamigoanimal.R;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
 
--- app/src/main/java/com/celvansystems/projetoamigoanimal/MainActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabCadastrar = findViewById(R.id.fabcadastrar);
+        fabCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
