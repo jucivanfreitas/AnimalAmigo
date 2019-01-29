467563a6f6a2d177e80463493a5f2054164e0d4f
Celio <celioalves10@hotmail.com>
2019-01-29 11:39:11 -0300

    vcs.xml corrigido

--- app/src/main/java/com/celvansystems/projetoamigoanimal/LoginActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/LoginActivity.java
@@ -87,7 +87,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                    attemptLogin();
             }
         });
 
