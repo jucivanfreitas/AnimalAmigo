1607737203c0cf97a13cda0563bf93c91a461124
Celio <celioalves10@hotmail.com>
2019-01-29 21:19:52 -0300

    autenticacao v1

---
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,40 @@
+package com.celvansystems.projetoamigoanimal.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference referenciaStorage;
+
+    //retorna a referencia do database
+    public static DatabaseReference getFirebase(){
+        if( referenciaFirebase == null ){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFirebase;
+    }
+
+    //retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if( referenciaAutenticacao == null ){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+
+    //Retorna instancia do FirebaseStorage
+    public static StorageReference getFirebaseStorage(){
+        if( referenciaStorage == null ){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenciaStorage;
+    }
+
+}
+
