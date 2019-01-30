dd5f95c188d78250c68adff50d9d1bc2d0d7ac2b
Celio <celioalves10@hotmail.com>
2019-01-30 03:08:56 -0300

    iniciada populacao dos spinners da CadastrarAnuncioActivity

--- app/src/main/java/com/celvansystems/projetoamigoanimal/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/activity/CadastrarAnuncioActivity.java
@@ -3,10 +3,18 @@ package com.celvansystems.projetoamigoanimal.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.celvansystems.projetoamigoanimal.R;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private Spinner spnEspecie, spnSexo, spnIdade, spnPorte, spnVacina;
+    private EditText edtDescricao;
+    private Button btnCadastrarAnuncio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,13 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastrar_anuncio);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        spnIdade = (Spinner) findViewById(R.id.spinner_cad_idade);
+        spnPorte = (Spinner) findViewById(R.id.spinner_cad_porte);
+        edtDescricao = (EditText) findViewById(R.id.editText_cad_descrição);
+        btnCadastrarAnuncio = (Button) findViewById(R.id.btnCadastrarAnuncio);
+
+        carregarDadosSpinner();
 
     //    FloatingActionButton fab = findViewById(R.id.fabcadastrar);
   //      fab.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +41,22 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-}
+    private void carregarDadosSpinner() {
+
+        //String [] estados = getResources().getStringArray(R.array.estados);
+
+        String [] idades = getResources().getStringArray(R.array.idade);
+        String [] portes = getResources().getStringArray(R.array.portes);
+
+        //idades
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, idades);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnIdade.setAdapter(adapter);
+
+        //portes
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, portes);
+        spnPorte.setAdapter(adapter);
+        };
+    }
+
+
