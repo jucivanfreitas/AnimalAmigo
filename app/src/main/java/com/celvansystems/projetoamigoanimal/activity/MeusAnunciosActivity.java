b4ccc0a6b44ac9f43a94962fb8736b7991e1e3a8
Celio <celioalves10@hotmail.com>
2019-01-31 21:29:11 -0300

    implementada meus anuncios

--- app/src/main/java/com/celvansystems/projetoamigoanimal/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/activity/MeusAnunciosActivity.java
@@ -4,17 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.celvansystems.projetoamigoanimal.R;
+import com.celvansystems.projetoamigoanimal.adapter.AdapterAnuncios;
+import com.celvansystems.projetoamigoanimal.helper.ConfiguracaoFirebase;
+import com.celvansystems.projetoamigoanimal.model.Animal;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerAnuncios;
+    private List<Animal> anuncios = new ArrayList<Animal>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
+
+        inicializarComponentes();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,6 +49,48 @@ public class MeusAnunciosActivity extends AppCompatActivity {
         });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // configuracoes iniciais
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_animais")
+                .child(ConfiguracaoFirebase.getIdUsuario());
+
+        //configurar recyclerview
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+        recyclerAnuncios.setAdapter(adapterAnuncios);
+
+        //recupera anuncios para o usuario
+        recuperarAnuncios();
+    }
+
+    private void recuperarAnuncios(){
+
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                anuncios.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    anuncios.add(ds.getValue(Animal.class));
+                }
+                Collections.reverse(anuncios);
+                adapterAnuncios.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void inicializarComponentes(){
+
+        recyclerAnuncios = (RecyclerView) findViewById(R.id.recycle_meus_anuncios);
+
     }
 
     public void abriCadastro(){
---
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/adapter/AdapterAnuncios.java
@@ -0,0 +1,77 @@
+package com.celvansystems.projetoamigoanimal.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.celvansystems.projetoamigoanimal.R;
+import com.celvansystems.projetoamigoanimal.model.Animal;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterAnuncios extends RecyclerView.Adapter<AdapterAnuncios.MyViewHolder> {
+
+
+    private List<Animal> anuncios;
+    private Context context;
+
+    public AdapterAnuncios() {
+
+    }
+
+    public AdapterAnuncios(List<Animal> anuncios, Context context) {
+        this.anuncios = anuncios;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View item = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_anuncio, viewGroup, false);
+        return new MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+        Animal anuncio = anuncios.get(i);
+        myViewHolder.especie.setText(anuncio.getEspécie());
+        myViewHolder.nome.setText(anuncio.getNome());
+        myViewHolder.idade.setText(anuncio.getIdade());
+
+        //pega a primeira imagem cadastrada
+        List<String> urlFotos = anuncio.getFotos();
+        String urlCapa = urlFotos.get(0);
+
+        Picasso.get().load(urlCapa).into(myViewHolder.foto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return anuncios.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView especie;
+        TextView nome;
+        TextView idade;
+        ImageView foto;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            especie = itemView.findViewById(R.id.textEspecie);
+            nome = itemView.findViewById(R.id.textNome);
+            idade = itemView.findViewById(R.id.textIdade);
+            foto = itemView.findViewById(R.id.imageAnuncio);
+        }
+    }
+}
