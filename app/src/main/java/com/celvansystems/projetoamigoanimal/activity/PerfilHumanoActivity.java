814d61120d1ebe39e1ce370e40255191a640d04d
jucivanfreitas <jucivan.fsantos@gmail.com>
2019-02-17 17:23:54 -0300

    Aperfeiçoamento de icons da Navbar Drawer layer

--- app/src/main/java/com/celvansystems/projetoamigoanimal/activity/PerfilHumanoActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/activity/PerfilHumanoActivity.java
@@ -1,16 +1,13 @@
 package com.celvansystems.projetoamigoanimal.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.celvansystems.projetoamigoanimal.R;
@@ -54,27 +51,27 @@ public class PerfilHumanoActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.perfil_humano, menu);
-        return true;
-    }
+   // @Override
+ //   public boolean onCreateOptionsMenu(Menu menu) {
+  //      // Inflate the menu; this adds items to the action bar if it is present.
+  //      getMenuInflater().inflate(R.menu.perfil_humano, menu);
+    //    return true;
+   // }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
+  //  @Override
+  //  public boolean onOptionsItemSelected(MenuItem item) {
+   //     // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+   //     int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+   //     //noinspection SimplifiableIfStatement
+    //    if (id == R.id.action_settings) {
+      //      return true;
+  //      }
 
-        return super.onOptionsItemSelected(item);
-    }
+    //    return super.onOptionsItemSelected(item);
+    //}
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -82,13 +79,19 @@ public class PerfilHumanoActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_perfil) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_config_notificacoes) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_meus_anuncios) {
+            startActivity(new Intent(getApplicationContext(),MeusAnunciosActivity.class));
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.cad_pet) {
+            startActivity(new Intent(getApplicationContext(), CadastrarAnuncioActivity.class));
+
+
+        } else if (id == R.id.doacao) {
+            startActivity(new Intent(getApplicationContext(), DoacaoActivity.class));
 
         } else if (id == R.id.nav_share) {
 
