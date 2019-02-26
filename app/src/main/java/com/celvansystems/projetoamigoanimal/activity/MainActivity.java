35b01375abfc57302c9a3a0365ed823c3ffd5fb7
Celio <celioalves10@gmail.com>
2019-02-26 20:37:08 -0300

    capitais colocadas como primeira opcao no filtro

--- app/src/main/java/com/celvansystems/projetoamigoanimal/activity/MainActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/activity/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil_humano);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         inicializarComponentes();
