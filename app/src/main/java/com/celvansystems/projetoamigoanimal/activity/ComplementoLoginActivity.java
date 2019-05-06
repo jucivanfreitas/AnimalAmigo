0d62861f23368aa7014e77a6c8832385ec699ab8
Celio <celioalves10@gmail.com>
2019-05-06 20:24:19 -0300

    navegacao entre paginas do complemento do cadastro

--- app/src/main/java/com/celvansystems/projetoamigoanimal/activity/ComplementoLoginActivity.java
+++ app/src/main/java/com/celvansystems/projetoamigoanimal/activity/ComplementoLoginActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +38,20 @@ public class ComplementoLoginActivity extends AppCompatActivity {
     private Button btnCadastrar;
     private View layout;
 
+    private ConstraintLayout layout_inserir_nome;
+    private ConstraintLayout layout_inserir_foto;
+    private ConstraintLayout layout_inserir_cidade;
+    private ConstraintLayout layout_inserir_fone;
+    private Button btn_proximo_nome;
+    private Button btn_proximo_fone;
+    private Button btn_proximo_foto;
+
+    private Button btn_voltar_foto;
+    private Button btn_voltar_fone;
+    private Button btn_voltar_cidade;
+
+    private Button btn_finalizar_complemento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,9 +75,79 @@ public class ComplementoLoginActivity extends AppCompatActivity {
         spnCidade = findViewById(R.id.spinner_cad_cidade_complemento);
         edtProfissao = findViewById(R.id.editText_cad_profissao);
         edtSobreMim = findViewById(R.id.editText_cad_sobremim);
-        btnCadastrar = findViewById(R.id.btn_cad_complemento);
+        btnCadastrar = findViewById(R.id.btn_finalizar_complemento);
+
+
+        btn_proximo_nome = findViewById(R.id.btn_proximo_nome);
+        btn_proximo_fone = findViewById(R.id.btn_proximo_fone);
+        btn_proximo_foto = findViewById(R.id.btn_proximo_foto);
+        btn_finalizar_complemento = findViewById(R.id.btn_finalizar_complemento);
+
+        btn_voltar_fone = findViewById(R.id.btn_volta_fone);
+        btn_voltar_foto = findViewById(R.id.btn_voltar_foto);
+        btn_voltar_cidade = findViewById(R.id.btn_voltar_cidade);
+
+        layout_inserir_nome = findViewById(R.id.layout_inserir_nome);
+        layout_inserir_foto = findViewById(R.id.layout_inserir_foto);
+        layout_inserir_cidade = findViewById(R.id.layout_inserir_cidade);
+        layout_inserir_fone = findViewById(R.id.layout_inserir_fone);
 
         edtNome.setText(ConfiguracaoFirebase.getFirebaseAutenticacao().getCurrentUser().getDisplayName());
+
+        btn_proximo_nome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_inserir_nome.setVisibility(View.INVISIBLE);
+                layout_inserir_fone.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_proximo_fone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_inserir_fone.setVisibility(View.INVISIBLE);
+                layout_inserir_foto.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_proximo_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_inserir_foto.setVisibility(View.INVISIBLE);
+                layout_inserir_cidade.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_finalizar_complemento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_voltar_fone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_inserir_nome.setVisibility(View.VISIBLE);
+                layout_inserir_fone.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btn_voltar_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_inserir_foto.setVisibility(View.INVISIBLE);
+                layout_inserir_fone.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_voltar_cidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_inserir_cidade.setVisibility(View.INVISIBLE);
+                layout_inserir_foto.setVisibility(View.VISIBLE);
+            }
+        });
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
